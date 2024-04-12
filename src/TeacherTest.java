0edbe98cdc2bd28da7e007eda9abc00d0ea7eb4f
Ali <alimahdavirad1383@gmail.com>
2024-04-12 17:22:06 +0330

    edit TeacherTest

--- src/TeacherTest.java
+++ src/TeacherTest.java
@@ -1,23 +1,81 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 public class TeacherTest {
     @Test
-    public void testRemoveCourse(){}
+    public void testRemoveCourse(){
+        Course course =new Course("Ap",3);
+        Teacher teacher = new Teacher();
+        teacher.AddCourse(course);
+        Assertions.assertEquals(teacher.courses.getLast(),course);
+        teacher.RemoveCourse(course);
+        Assertions.assertEquals(teacher.courses,new ArrayList<>());
+    }
     @Test
-    public void testRemoveStudent(){}
+    public void testRemoveStudent(){
+        Course course =new Course("Ap",3);
+        Teacher teacher = new Teacher();
+        Student student = new Student();
+        teacher.AddCourse(course);
+        student.newTerm();
+        teacher.AddStudent(course,student);
+        Assertions.assertEquals(student.terms.getLast().studentCourses.getLast(),course.studentCourseVersion);
+        teacher.RemoveStudent(course,student);
+        Assertions.assertEquals(student.terms.getLast().studentCourses,new ArrayList<>());
+        }
     @Test
-    public void testAddAssignment(){}
+    public void testAddAssignment(){
+        Course course =new Course("Ap",3);
+        Teacher teacher = new Teacher();
+        Student student = new Student();
+        teacher.AddCourse(course);
+        Assignment assignment =new Assignment("Junit Exercise");
+        teacher.AddAssignment(course,assignment);
+        teacher.AssignmentActivator(course,assignment);
+        Assertions.assertEquals(course.ActiveAssignments.getLast(),assignment);
+        Assertions.assertEquals(course.AllAssignments.getLast(),assignment);
+    }
     @Test
-    public void testRemoveAssignment(){}
+    public void testRemoveAssignment(){
+        Course course =new Course("Ap",3);
+        Teacher teacher = new Teacher();
+        Student student = new Student();
+        teacher.AddCourse(course);
+        Assignment assignment =new Assignment("Junit Exercise");
+        Assignment assignment2 =new Assignment("oop");
+        Assignment assignment3 =new Assignment("Exception");
+        teacher.AddAssignment(course,assignment);
+        teacher.AddAssignment(course,assignment2);
+        teacher.AddAssignment(course,assignment3);
+        ArrayList<Assignment>result=new ArrayList<>();
+        result.add(assignment);
+        result.add(assignment2);
+        result.add(assignment3);
+        teacher.AssignmentActivator(course,assignment);
+        teacher.AssignmentActivator(course,assignment2);
+        teacher.AssignmentActivator(course,assignment3);
+        Assertions.assertEquals(course.ActiveAssignments,result);
+        Assertions.assertEquals(course.AllAssignments,result);
+        teacher.RemoveAssignment(course,assignment);
+        Assertions.assertEquals(course.AllAssignments,result);
+        result.remove(assignment);
+        Assertions.assertEquals(course.ActiveAssignments,result);
+    }
     @Test
-    public void testDeadLineTimeEdit(){}
-    @Test
-    public void testAssignmentActivator(){}
+    public void testDeadLineTimeEdit(){
+        Course course =new Course("Ap",3);
+        Teacher teacher = new Teacher();
+        Student student = new Student();
+        teacher.AddCourse(course);
+        Assignment assignment =new Assignment("Junit Exercise");
 
+        teacher.AddAssignment(course,assignment);
+        teacher.AssignmentActivator(course,assignment);
+
+        teacher.DeadLineTimeEdit(course,assignment,new Date(2024,4,12));
+        Assertions.assertEquals(assignment.deadLine,new Date(2024,4,12));
+
+    }
 }
